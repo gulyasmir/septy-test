0803349ea1edd29cd4bccc126926ad8415da2641
gulyasmir <gulyasmir@yandex.ru>
2021-11-15 17:45:23 +0300

    auth login

--- store/auth.js
+++ store/auth.js
@@ -20,7 +20,7 @@ export const mutations = {
 export const actions = {
   async login({commit, dispatch}, payload) {
     try {
-      const url=`https://septytest.herokuapp.com/api/login?email=${payload.email}&password=${payload.password}`
+      const url=`http://localhost:3000/api/login?email=${payload.email}&password=${payload.password}`
       const resultdata = await this.$axios.post(url)
       console.log('login result', resultdata.data)
       const token = resultdata.data.token
