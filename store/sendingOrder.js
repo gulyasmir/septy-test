25b22e4d4b3ab564bd3fd8104b00a4e731994206
gulyasmir <gulyasmir@yandex.ru>
2021-11-25 12:41:15 +0300

    new api

--- store/sendingOrder.js
+++ store/sendingOrder.js
@@ -1,14 +1,11 @@
 export const actions = {
   async sendingData({commit}, payload) {
     try {
-      const url = `https://septy-test-api.herokuapp.com/api/orders?email=${payload.email}&name=${payload.name}&text=${payload.text}`
+      const url = `http://api.chymes.ru/api/orders?email=${payload.email}&name=${payload.name}&text=${payload.text}`
 
       let resultdata = await this.$axios.post(url)
-
-      console.log('sendingData result', resultdata)
       return resultdata.data
     } catch (e) {
-      console.log('sendingData store error')
       throw  e
     }
   }
